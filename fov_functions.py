7af886f595d1399d1b2ba6c3fb1d4cfeebfa00a0
dbane744 <danielbane@hotmail.co.uk>
2017-09-26 11:40:02 +0100

    Fixed a major bug that prevented the FOV properties for each tile being set

--- fov_functions.py
+++ fov_functions.py
@@ -15,8 +15,7 @@ def initialise_fov(game_map):
         for x in range(game_map.width):
             libtcod.map_set_properties(fov_map, x, y, not game_map.tiles[x][y].block_sight,
                                        not game_map.tiles[x][y].blocked)
-
-        return fov_map
+    return fov_map
 
 
 def recompute_fov(fov_map, x, y, radius, light_walls=True, algorithm = 0):
